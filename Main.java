0b02b60ce2f02155def6c18a4e314847041d11f5
Alexis Giust <alx.giust@gmail.com>
2021-03-11 17:26:47 +0100

    Added getPlayer() method + added check that the cell is empty before adding mark + rename Pion -> Mark

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 class Main {
 
     public static void main(String[] args) {
-        TTT jp = new TTT(TTT.Pion.CIRCLE, TTT.Pion.CROSS);
+        TTT jp = new TTT();
+        System.out.println(jp);
+        jp.play(0, 0);
+        System.out.println(jp);
+        jp.play(0, 0);
+        System.out.println(jp);
+        jp.play(0, 1);
+        System.out.println(jp);
     }
 
 }
--- TTT.java
+++ TTT.java
@@ -10,25 +10,27 @@ public class TTT {
         }
     }
 
+    public static final Pion PLAYER1 = Pion.CROSS;
+    public static final Pion PLAYER2 = Pion.CIRCLE;
+
     private final Pion[][] grid = new Pion[3][3];
     private int turn = 0;
-    private Pion player1, player2;
 
-    public TTT(Pion p1, Pion p2) {
+    public TTT() {
         for (int x = 0; x < 3; x++) {
             for (int y = 0; y < 3; y++) {
                 grid[x][y] = Pion.EMPTY;
             }
         }
-
-        player1 = p1;
-        player2 = p2;
     }
 
     public TTT(TTT ttt) {
-        for (int x = 0; x < 3; x++)
-            for (int y = 0; y < 3; y++)
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
                 grid[x][y] = ttt.grid[x][y];
+            }
+        }
+        this.turn = ttt.turn;
     }
 
     public void print() {
@@ -43,33 +45,43 @@ public class TTT {
         System.out.println("-------------");
     }
 
-    public void play(int x, int y){
-        if (turn < 9){
-            grid[y][x] = turn % 2 == 0 ? player1 : player2;
+    public void play(int x, int y) {
+        if (turn < 9 && grid[x][y] == Pion.EMPTY) {
+            grid[y][x] = getPlayer();
 
-            print();
+            // C'est mieux d'afficher dans la boucle principale non ?
+//            print();
 
-            if (check()){
+            if (check()) {
                 // Code if won
                 System.out.println("The " + grid[y][x] + " won!");
-            }
-            else if (turn == 8){
+            } else if (turn == 8) {
                 // Code if draw
                 System.out.println("It is a draw!");
             }
-            else {turn++;}
+            turn++;
         }
     }
 
-    private boolean check(){
+    /**
+     * @return The player whose turn it is to play.
+     */
+    public Pion getPlayer() {
+        return turn % 2 == 0 ? PLAYER1 : PLAYER2;
+    }
+
+    /**
+     * @return true if there is a winner, false otherwise.
+     */
+    private boolean check() {
         // Check diagonals
-        if (grid[0][0] != Pion.EMPTY && grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] || grid[0][2] != Pion.EMPTY && grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0]){
+        if (grid[0][0] != Pion.EMPTY && grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] || grid[0][2] != Pion.EMPTY && grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0]) {
             return true;
         }
 
         for (int i = 0; i < 3; i++) {
             // Check lines and columns
-            if (grid[i][0] != Pion.EMPTY && grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2] || grid[0][i] != Pion.EMPTY && grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i]){
+            if (grid[i][0] != Pion.EMPTY && grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2] || grid[0][i] != Pion.EMPTY && grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i]) {
                 return true;
             }
 
